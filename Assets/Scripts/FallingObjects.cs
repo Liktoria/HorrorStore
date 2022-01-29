d3cefae70cb4df0a11d0a35e5ef0ecb13190db2d
HerrVorragendTheReal <gamer99email@gmail.com>
2022-01-29 13:03:14 +0100

    Added Birger Scene

---
+++ Assets/Scripts/FallingObjects.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingObjects : MonoBehaviour
+{
+    public GameObject fallingObject;
+    public Vector3 fallingForceStrength;
+    public bool isObjectFallingDown;
+    public float eventProbability;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // only call with given probability
+        float probability = Random.Range(0f, 1f);
+
+        if (probability < eventProbability)
+        {
+            if (isObjectFallingDown)
+                fallingObject.GetComponent<Rigidbody>().useGravity = true;
+
+            fallingObject.GetComponent<Rigidbody>().AddForce(fallingForceStrength, ForceMode.Impulse);
+
+            gameObject.SetActive(false);
+        }
+    }
+}
