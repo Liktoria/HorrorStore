4265ccd36f23136e9587e63a4cfc62b52b3f740b
Liktoria <tsunamy@gmx.de>
2022-01-30 18:30:50 +0100

    Fixes

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -12,7 +12,6 @@ public class Music : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(this.gameObject);
         GameManager.gameManager.LightSwitched += LightSwitched;
     }
 
